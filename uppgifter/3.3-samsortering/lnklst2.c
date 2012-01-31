1ecfb79d9f7c2cd7ffa372bac953ca5076b84942
Simon Kers <skers@kth.se>
2012-01-31 21:06:16 +0100

    nästan klar

--- uppgifter/3.3-samsortering/lnklst2.c
+++ uppgifter/3.3-samsortering/lnklst2.c
@@ -7,6 +7,9 @@ typedef struct namntyp {
 	struct namntyp *nasta;
 } namntyp;
 
+namntyp *head;
+namntyp *tail;
+
 void laes_in(namntyp **l1, char filnamn[])
 {
 	FILE *fil;
@@ -17,7 +20,7 @@ void laes_in(namntyp **l1, char filnamn[])
 	fil = fopen(filnamn, "rt");
 	*l1 = NULL;
 	while(!feof(fil)) {
-		fscanf(fil, "%s",namn);
+		fscanf(fil, "%s", namn);
 		if (!feof(fil)) {
 			if (*l1 != NULL)
 				forra = a;
@@ -33,13 +36,27 @@ void laes_in(namntyp **l1, char filnamn[])
 	fclose(fil);
 }
 
+void append(namntyp **sl, namntyp *l1)
+{
+	namntyp *new;
+	new = (namntyp *)malloc(sizeof(namntyp));
+	strcpy(new->namn, l1->namn);
+
+	tail->nasta = new;
+	tail = tail->nasta;
+}
+
 void merge(namntyp *l1, namntyp *l2, namntyp **sl)
 {
-	printf("%s\t", l1->namn);
-	printf("%s\n", l2->namn);
-	printf("strcmp: %d\n", strcmp(l1->namn, l2->namn));
-	if (strcmp(l1->namn, l2->namn) < 0)
-		printf("%s\n", l1->namn);
+	if (strcmp(l1->namn, l2->namn) <= 0) {
+		append(sl, l1);
+		if (l1->nasta != NULL)
+			merge(l1->nasta, l2, sl);
+	} else if (strcmp(l1->namn, l2->namn) > 0) {
+		append(sl, l2);
+		if (l2->nasta != NULL)
+			merge(l1, l2->nasta, sl);
+	}
 }
 
 void skriv_ut(namntyp *sl)
@@ -57,15 +74,17 @@ int main(void)
 {
 	namntyp *lista1;
 	namntyp *lista2;
-	namntyp *slutlista;
+	namntyp *slutlista = (namntyp*)malloc(sizeof(namntyp)*20);
+	head = slutlista;
+	tail = slutlista;
+
 	laes_in(&lista1, "UPPG3A.DAT");
 	laes_in(&lista2, "UPPG3B.DAT");
 	merge(lista1, lista2, &slutlista);
-	//laes_in(&lista1, "UPPG3C.DAT");
-	//laes_in(&lista2, "UPPG3D.DAT");
-	//merge(lista1, lista2, &slutlista);
-	//laes_in(&lista1, "UPPG3E.DAT");
-	//skriv_ut(slutlista);
+
+	skriv_ut(slutlista);
+	free(lista1);
+	free(lista2);
 
 	return 0;
 }
