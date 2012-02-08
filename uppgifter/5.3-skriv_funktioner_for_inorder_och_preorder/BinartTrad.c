5d9e8465b5a4698d7be8bbf40175ccab9cb262ed
Simon Kers <skers@kth.se>
2012-02-08 01:59:20 +0100

    5.3 ez zzzzzzz

---
+++ uppgifter/5.3-skriv_funktioner_for_inorder_och_preorder/BinartTrad.c
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct objekt {
+	char nyckel[10];
+	struct objekt *left, *right;
+};
+
+typedef struct objekt objekt;
+
+objekt *insert(objekt * start, objekt ny)
+{
+	objekt *x, *y, *z;
+	z = (objekt *) malloc(sizeof(objekt));
+	*z = ny;
+	y = NULL;
+	x = start;
+	while (x != NULL) {
+		y = x;
+		if (strcmp(z->nyckel, x->nyckel) < 0)
+			x = x->left;
+		else
+			x = x->right;
+	}
+	if (y == NULL)
+		start = z;
+	else {
+		if (strcmp(z->nyckel, y->nyckel) < 0)
+			y->left = z;
+		else
+			y->right = z;
+	}
+	return start;
+}
+
+// Den här funktionen ska du skriva
+void inorder(objekt * start)
+{
+}
+
+void postorder(objekt * start)
+{
+	objekt *x;
+	x = start;
+	if (x != NULL) {
+		postorder(x->left);
+		postorder(x->right);
+		printf("%s ", x->nyckel);
+	}
+}
+
+// Den här funktionen ska du skriva
+void preorder(objekt * start)
+{
+}
+
+void main(void)
+{
+	objekt *start, ny;
+	FILE *fil;
+	int klar = 0;
+	char namn[10];
+
+	start = NULL;
+	fil = fopen("bintrad3.dat", "rt");
+	while (!klar) {
+		fscanf(fil, "%s", namn);
+		if (!feof(fil)) {
+			strcpy(ny.nyckel, namn);
+			ny.left = NULL;
+			ny.right = NULL;
+			start = insert(start, ny);
+		} else
+			klar = 1;
+	}
+	fclose(fil);
+	preorder(start);
+}
